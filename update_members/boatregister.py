34b0d422822d2b4b7c13ef603423dc47d6320d5e
Julian Cable <julian.cable@yahoo.com>
2025-10-05 12:02:10 +0100

    Update boatregister.py

--- update_members/boatregister.py
+++ update_members/boatregister.py
@@ -1,2 +1,8 @@
-def update(before, after)
-  pass
+def update(before, after=None)
+  if after:
+    print('TODO UPDATE boat register (may be noop)')
+    return
+  print('TODO update boat owner records')
+  
+  def delete(member):
+    print('TODO replace gold id with owner name')
