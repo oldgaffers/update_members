36ed66085896637720a64af116877d769aeb9d08
jcable <boatregister@oga.org.uk>
2025-10-05 15:29:27 +0100

    fix: tidy

--- update_members/auth0.py
+++
@@ -1,8 +0,0 @@
-def update(before, after=None)
-  if after:
-    print('TODO UPDATE auth0')
-    return
-  print('TODO Add roles and metadata in auth0')
-  
-  def delete(member):
-    print('TODO delete member from auth0')
--- update_members/boatregister.py
+++
@@ -1,8 +0,0 @@
-def update(before, after=None)
-  if after:
-    print('TODO UPDATE boat register (may be noop)')
-    return
-  print('TODO update boat owner records')
-  
-  def delete(member):
-    print('TODO replace gold id with owner name')
