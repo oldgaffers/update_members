9d857491c8ca333b7638030f412c7cb7b7b03516
Julian Cable <julian.cable@yahoo.com>
2025-10-05 11:59:58 +0100

    Update auth0.py

--- update_members/auth0.py
+++ update_members/auth0.py
@@ -1,2 +1,8 @@
-def update(before, after)
-  pass
+def update(before, after=None)
+  if after:
+    print('TODO UPDATE auth0')
+    return
+  print('TODO Add roles and metadata in auth0')
+  
+  def delete(member):
+    print('TODO delete member from auth0')
